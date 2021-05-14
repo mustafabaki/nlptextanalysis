7aed75bcb63a007681cf7bd747ca07ca428a6f60
mustafabaki <83211751+mustafabaki@users.noreply.github.com>
2021-05-14 23:58:25 +0300

    Added Naive Bayes Algorithm and Improvements on UI

---
+++ naivebayes.py
@@ -0,0 +1,49 @@
+from sklearn.datasets import fetch_20newsgroups
+
+data = fetch_20newsgroups()
+data.target_names
+
+categories = ['alt.atheism',
+ 'comp.graphics',
+ 'comp.os.ms-windows.misc',
+ 'comp.sys.ibm.pc.hardware',
+ 'comp.sys.mac.hardware',
+ 'comp.windows.x',
+ 'misc.forsale',
+ 'rec.autos',
+ 'rec.motorcycles',
+ 'rec.sport.baseball',
+ 'rec.sport.hockey',
+ 'sci.crypt',
+ 'sci.electronics',
+ 'sci.med',
+ 'sci.space',
+ 'soc.religion.christian',
+ 'talk.politics.guns',
+ 'talk.politics.mideast',
+ 'talk.politics.misc',
+ 'talk.religion.misc']
+
+
+train = fetch_20newsgroups(subset='train', categories=categories)
+test = fetch_20newsgroups(subset='test', categories=categories)
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.pipeline import make_pipeline
+
+model = make_pipeline(TfidfVectorizer(), MultinomialNB())
+
+model.fit(train.data, train.target)
+labels = model.predict(test.data)
+
+# this function is used to predict the given text's topic.
+def predict_category(s):
+   
+    pred = model.predict([s])
+    
+    
+    
+    return train.target_names[pred[0]]
+
+
