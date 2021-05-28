cd68632871e0f32d5207f0c494ca52e4361af1f2
DejiFN <deji626@gmail.com>
2021-05-28 04:41:11 +0300

    user login and password hash functiom

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request,redirect, url_for,send_from_directory
 from flask import send_file
-import os
-
+from flask_bootstrap import Bootstrap
+from flask_login.utils import logout_user
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import  LoginManager, UserMixin, login_user, login_required, current_user
 from flask.globals import current_app
+import os
 import word_cloud
 import naivebayes
 import nltk 
@@ -14,16 +21,117 @@ import pam_eng
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 picFolder = os.path.join('static', 'pics')
 app.config['EXCEL_FILES'] = os.path.join('static', 'excelfiles')
 app.config['UPLOAD_FOLDER'] = picFolder
+app.config["SECRET_KEY"] = "any string works here"
+# location of the database/ you should change this to your relative path directory 
+app.config[
+    "SQLALCHEMY_DATABASE_URI"
+] = "sqlite:///C:\\Users\\deji6\\OneDrive\\Desktop\\nlptextanalysis\\database.db"
 
+
+# initializing the database and using  helper tools
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+# defines the database for storing user details  / creating the model
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(15), unique=True)
+    email = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+   return User.query.get(int(user_id))
+
+
+def __repr__(self):
+    return "<username %r >" % self.id
+
+# login class
+class LoginForm(FlaskForm):
+    username = StringField(
+        "username", validators=[InputRequired(), Length(min=4, max=15)]
+    )
+    password = PasswordField(
+        "password", validators=[InputRequired(), Length(min=8, max=80)]
+    )
+    remember = BooleanField("remember me")
+
+# sign up class
+class RegisterForm(FlaskForm):
+    email = StringField(
+        "email",
+        validators=[InputRequired(), Email(message="Invalid email"), Length(max=50)],
+    )
+    username = StringField(
+        "username", validators=[InputRequired(), Length(min=4, max=15)]
+    )
+    password = PasswordField(
+        "password", validators=[InputRequired(), Length(min=8, max=80)]
+    )
+
+     # login function / where i compare the hashed password
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if  check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+        return  '<h1> Invaild username or password </h1>'
+        #return "<h1>" + form.username.data + " " + form.password.data + "</h1>"
+    return render_template("login.html", form=form)
+
+
+# sign up function/ where the passwords are hashed
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method= 'sha256')
+        new_user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password,
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        return "<h1> New user has been created! <!h1>"
+        # return '<h1>' + form.username.data + ' ' + form.email.data + ' ' + form.password.data + '</h1>'
+    return render_template("signup.html", form=form)
+
+
+# routes the user into the application with login check
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('index.html', name=current_user.username)
+
+# routes the user into the application without login check
+@app.route('/free')
+def free():
+    return render_template('index.html', name= "Guest User")
+
+ # sign the user out 
+@app.route('/logout')
+@login_required
+def logout():
+   logout_user()
+   return redirect(url_for('login'))
 
 @app.route('/')
 
 def index():
     
-    return render_template("index.html")
+    return render_template("index2.html")
 
 @app.route('/', methods =['POST'])
 def test():
@@ -33,9 +141,6 @@ def test():
   
     f.save(os.path.join("uploads",f.filename))
     word_cloud.cloudify(f.filename)
-
-
-
 
     outname = f.filename+"_out.png"
     pic1 = os.path.join(app.config['UPLOAD_FOLDER'], outname)
