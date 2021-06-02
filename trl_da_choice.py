abe07fe7e0805c515f508838862edd67bc5a1eb0
Bahadir Akdemir <74659903+BahadirAkdemir@users.noreply.github.com>
2021-06-02 15:33:04 +0300

    Additional Changes For LDA (User Dataset)

--- trl_da_choice.py
+++ trl_da_choice.py
@@ -1,7 +1,7 @@
 def make_ngrams(texts,n,ngram_mod):
     return [turnmod(doc,n,ngram_mod) for doc in texts]
 
-def lemmatization(texts, allowed_postags=["Noun", 'Adj', 'Verb', 'Adv']):
+def lemmatization(texts):
       texts_out = []
       for sent in texts:
           x=analyzer.analyze(sent)[0][0]
@@ -31,7 +31,8 @@ def prepare_stopwords(link='C:/Users/akdem/Desktop/nlptextanalysis-main/stopword
     stopwords.append(i[0])
   return stopwords
 
-from multiprocessing import Process, freeze_support
+from multiprocessing import Process, freeze_support, Queue
+from multiprocessing.queues import Queue
 import pandas as pd
 import pickle
 import zeyrek
@@ -55,32 +56,27 @@ import numpy as np
 
 
 
-def f():
-    open_file = open("C:/Users/akdem/Desktop/nlptextanalysis-main/test.pkl", "rb")
-    loaded_list = pickle.load(open_file)
-    open_file.close()
-    df=loaded_list
-
-
-    print('hello world!')
+def f(DATA_LINK, DATA_COLUMN_NAME, STOPWORD_CHOICE, STOPWORD_LINK, NGRAM_CHOICE,NGRAM_NUM, TestData,fetchArray):
+    data = pd.read_csv(DATA_LINK)
+    df=data[DATA_COLUMN_NAME]
     ######################################################################
-    if (False):
-        stopwords=prepare_stopwords('FILE NAME')
+    if (STOPWORD_CHOICE):
+        stopwords=prepare_stopwords(STOPWORD_LINK)
     else:
         stopwords=prepare_stopwords()
     ######################################################################
-    print("stop bitti")
+
     df=clean(df)
-    print("clean bitti")
+
     processed_docs = []
 
     for doc in df:
         processed_docs.append(preprocess(doc,stopwords))
     ############################################################################
-    if True:
+    if NGRAM_CHOICE:
         ngram=[]
         ngram_mod=[]
-        for i in range(3):
+        for i in range(NGRAM_NUM):
             if(i==0):
                 ngram.append(gensim.models.Phrases(processed_docs[0:10000], min_count=5, threshold=100)) # higher threshold fewer phrases
             else:
@@ -90,43 +86,52 @@ def f():
     ###########################################################################
 
     ################################################################################
-    if True:
+    if NGRAM_CHOICE:
     # Form Ngrams
-        data_words_ngrams = make_ngrams(processed_docs,3,ngram_mod)
-        print("ngram bitti")
+        data_words_ngrams = make_ngrams(processed_docs,NGRAM_NUM,ngram_mod)
+
         # Do lemmatization keeping only noun, adj, vb, adv
         data_lemmatized=[]
         for i in range(len(data_words_ngrams)):
-            data_lemmatized.append(lemmatization(data_words_ngrams[i], allowed_postags=['Noun', 'Adj', 'Verb', 'Adv']))
+            data_lemmatized.append(lemmatization(data_words_ngrams[i]))
     else:
         data_lemmatized=processed_docs
     ################################################################################
-    print("lemmatize bitti")
+  
 
     dictionary = gensim.corpora.Dictionary(data_lemmatized)
-    print("a")
-    dictionary.filter_extremes(no_below=2, no_above=0.2, keep_n= 100000)
-    print("b")
+
+    dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
+
     bow_corpus = [dictionary.doc2bow(doc) for doc in data_lemmatized]
-    print("c")
+
     lda_model =  gensim.models.LdaMulticore(bow_corpus, 
                                    num_topics = 70, 
                                    id2word = dictionary,                                    
                                    passes = 10, workers = 2)
-    print("lda bitti")
+
     for idx, topic in lda_model.print_topics(-1):
         print("Topic: {} \nWords: {}".format(idx, topic ))
         print("\n")
     lda_model.save('turk_lda.gensim')
 
-    unseen_document = "Çin Cengiz Han"
-    print(unseen_document)
+    unseen_document = TestData
+
+    rx = re.compile('\W+')
+    unseen_document = rx.sub(' ', unseen_document).strip()
+
 
     # Data preprocessing step for the unseen document
     bow_vector = dictionary.doc2bow(preprocess(unseen_document,stopwords))
 
+    topics = []
     for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
-        print("Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5)))
+      print("Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5)))
+      # rslt = result(str(score), str(lda.print_topic(index,5)))
+      rslt = result(str(score), str(re.findall('"([^"]*)"', str(lda_model.print_topic(index,5)))))
+      topics.append(rslt)
+
+    fetchArray.put(topics)
 
 def turnmod(text,n,ngram_mod):
     data_gram=ngram_mod[0][text]
@@ -142,7 +147,11 @@ def preprocess(text,stopwords):
      
     return result
 
+fetchArray=Queue()
 if __name__ == '__main__':
     freeze_support()
-    Process(target=f).start()
+    p1=Process(target=f,args=(DATA_LINK, DATA_COLUMN_NAME, STOPWORD_CHOICE, STOPWORD_LINK, NGRAM_CHOICE,NGRAM_NUM, TestData, fetchArray))
+    p1.start()
+    p1.join()
+    topics=fetchArray.get()
 
