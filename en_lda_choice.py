17eb1b4b6ff6476fce94ffdd5680429659c638f5
Bahadir Akdemir <74659903+BahadirAkdemir@users.noreply.github.com>
2021-06-03 16:59:24 +0300

    Add English LDA with User Data

---
+++ en_lda_choice.py
@@ -0,0 +1,155 @@
+class result:
+    def __init__(self, topic, score):
+        self.topic = topic
+        self.score = score
+
+def make_ngrams(texts,n,ngram_mod):
+    return [turnmod(doc,n,ngram_mod) for doc in texts]
+
+def lemmatization(texts,nlp):
+    texts_out = []
+    for sent in texts:
+        doc = nlp(" ".join(sent)) 
+        texts_out.append([token.lemma_ for token in doc])
+    return texts_out
+
+
+
+def clean(df):
+  df.fillna('').astype(str)
+  df=df.astype(str)
+  df = df.map(lambda x: re.sub('[,\.!?();:$%&#"]', '', x))
+  df = df.replace('\n','', regex=True)                     # depends on the data
+  df = df.replace('\'','', regex=True)                     # depends on the data
+  df = df.replace('-','', regex=True)                      # depends on the data
+  df = df.replace('’','', regex=True)
+  return df 
+
+def prepare_stopwords(link='C:/Users/akdem/Desktop/nlptextanalysis-main/stopwords.csv'):
+  stop_word_list=pd.read_csv(link)
+  stop_word_list=stop_word_list.values.tolist()
+  stopwords=[]
+  for i in stop_word_list:
+    stopwords.append(i[0])
+  return stopwords
+
+from multiprocessing import Process, freeze_support, Queue
+import pandas as pd
+import pickle
+import spacy
+import nltk
+#nltk.download('punkt')
+import numpy as np
+import nltk
+import re
+import warnings
+from collections import Counter
+import nltk
+import gensim
+from gensim.utils import simple_preprocess
+#from gensim.parsing.preprocessing import STOPWORDS           #Does not support Turkish yet.
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import *
+import numpy as np
+
+
+
+
+
+def f(DATA_LINK, DATA_COLUMN_NAME, STOPWORD_CHOICE, STOPWORD_LINK, NGRAM_CHOICE,NGRAM_NUM, TestData,fetchArray):
+    data = pd.read_csv(DATA_LINK)
+    df=data[DATA_COLUMN_NAME]
+    ######################################################################
+    if (STOPWORD_CHOICE):
+        stopwords=prepare_stopwords(STOPWORD_LINK)
+    else:
+        stopwords=prepare_stopwords()
+    ######################################################################
+
+    df=clean(df)
+
+    processed_docs = []
+
+    for doc in df:
+        processed_docs.append(preprocess(doc,stopwords))
+    ############################################################################
+    if NGRAM_CHOICE:
+        ngram=[]
+        ngram_mod=[]
+        for i in range(NGRAM_NUM):
+            if(i==0):
+                ngram.append(gensim.models.Phrases(processed_docs[0:10000], min_count=5, threshold=100)) # higher threshold fewer phrases
+            else:
+                ngram.append(gensim.models.Phrases(ngram[i-1][processed_docs[0:10000]], min_count=5, threshold=100)) # higher threshold fewer phrases
+            ngram_mod.append(gensim.models.phrases.Phraser(ngram[i]))
+    
+    ###########################################################################
+
+    ################################################################################
+    if NGRAM_CHOICE:
+    # Form Ngrams
+        data_words_ngrams = make_ngrams(processed_docs,NGRAM_NUM,ngram_mod)
+
+        # Do lemmatization keeping only noun, adj, vb, adv
+        nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
+        data_lemmatized = lemmatization(data_words_ngrams,nlp)
+    else:
+        data_lemmatized=processed_docs
+    ################################################################################
+  
+
+    dictionary = gensim.corpora.Dictionary(data_lemmatized)
+
+    dictionary.filter_extremes(no_below=15, no_above=0.1, keep_n= 100000)
+
+    bow_corpus = [dictionary.doc2bow(doc) for doc in data_lemmatized]
+
+    lda_model =  gensim.models.LdaMulticore(bow_corpus, 
+                                   num_topics = 70, 
+                                   id2word = dictionary,                                    
+                                   passes = 10, workers = 2)
+
+    for idx, topic in lda_model.print_topics(-1):
+        print("Topic: {} \nWords: {}".format(idx, topic ))
+        print("\n")
+
+    unseen_document = TestData
+
+    rx = re.compile('\W+')
+    unseen_document = rx.sub(' ', unseen_document).strip()
+
+
+    # Data preprocessing step for the unseen document
+    bow_vector = dictionary.doc2bow(preprocess(unseen_document,stopwords))
+
+    topics = []
+    for index, score in sorted(lda_model[bow_vector], key=lambda tup: -1*tup[1]):
+      print("Score: {}\t Topic: {}".format(score, lda_model.print_topic(index, 5)))
+      # rslt = result(str(score), str(lda.print_topic(index,5)))
+      rslt = result(str(score), str(re.findall('"([^"]*)"', str(lda_model.print_topic(index,5)))))
+      topics.append(rslt)
+
+    fetchArray.put(topics)
+
+def turnmod(text,n,ngram_mod):
+    data_gram=ngram_mod[0][text]
+    for i in range(n-1):
+      data_gram=ngram_mod[i+1][data_gram]
+    return data_gram
+
+def preprocess(text,stopwords):
+    result=[]
+    for token in gensim.utils.simple_preprocess(text) :
+        if token not in stopwords and len(token) > 3:
+            result.append(token)
+     
+    return result
+
+fetchArray=Queue()
+if __name__ == '__main__':
+    freeze_support()
+    p1=Process(target=f,args=(DATA_LINK, DATA_COLUMN_NAME, STOPWORD_CHOICE, STOPWORD_LINK, NGRAM_CHOICE,NGRAM_NUM, TestData, fetchArray))
+    p1.start()
+    p1.join()
+    topics=fetchArray.get()
+
