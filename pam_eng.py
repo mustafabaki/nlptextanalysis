0fac242c9311bcdafc0560496689311ca1e5a184
Batukan Demirtaş <76396946+batukan99@users.noreply.github.com>
2021-06-07 22:01:02 +0300

    Optimized and added ngrams

--- pam_eng.py
+++ pam_eng.py
@@ -6,22 +6,20 @@ def pam_english(input_file):
     print("importing...")
     from nltk.stem.porter import PorterStemmer
     from nltk.corpus import stopwords
+    
     stemmer = PorterStemmer()
     stops = set(stopwords.words('english'))
-    print("preparing corpus...")
+
     train_corpus = tp.utils.Corpus(tokenizer=tp.utils.SimpleTokenizer(stemmer=stemmer.stem),
                                    stopwords=lambda x: len(x) <= 2 or x in stops)
 
     # data_feeder yields a tuple of (raw string, user data) or a str (raw string)
     train_corpus.process(open(input_file, encoding='utf-8'))
-
     # make PA model and train
     print("training model...")
-    mdl = tp.PAModel(k1=5, k2=50, min_cf=10, min_df=1, corpus=train_corpus, seed=42)
-    mdl.train(0)
-    print('Num docs:', len(mdl.docs), ', Vocab size:', len(mdl.used_vocabs), ', Num words:', mdl.num_words)
-    print('Removed top words:', mdl.removed_top_words)
-    for i in range(0, 100, 10):  # increase 100 for more accurate results, but it will take more time
+    mdl = tp.PAModel(k1=5, k2=5, min_cf=2, min_df=1, corpus=train_corpus)
+
+    for i in range(0, 250, 10):  # increase 100 for more accurate results, but it will take more time
         mdl.train(10)
         print('Iteration: {}\tLog-likelihood: {}'.format(i, mdl.ll_per_word))
 
@@ -29,6 +27,12 @@ def pam_english(input_file):
     # save pam for reuse
     mdl.save('trained_pam.bin')
     # for loading use mdl.load('trained_pam.bin')
+
+    # Creating ngrams, max_len determines bigram or trigram, 3 means trigram
+    ngrams = train_corpus.extract_ngrams(min_cf=2, max_len=3)
+    for c in ngrams:
+        if len(c.words) == 3:
+            print(c.words[0], c.words[1], c.words[2], sep='\t') # ngram words
 
     topic_result = []
     for k in range(mdl.k1):
@@ -41,12 +45,11 @@ def pam_english(input_file):
                 subs.append(word)
             subs_prob.append(probability)
         for word, prob in mdl.get_topic_words(k, top_n=1):
-            print(word, prob, sep='\t')
             topic_result.append({"topic": word, "prob": prob, "subs": subs, "subs_prob": subs_prob})
 
     return topic_result
 
-""""
+
 print('Running PAM from raw corpus')
 topics = pam_english('enwiki-1000.txt')
 # example for accessing values
@@ -55,7 +58,6 @@ topics = pam_english('enwiki-1000.txt')
 # subs_prob represents list 5 sub topics probabilities
 # first element of subs_prob corresponds to first element of subs
 # probabilities for the main topics are unsorted
-for w in range(len(topics)):
-    print("\n")
-    print(topics[w]["topic"], topics[w]["prob"], topics[w]["subs"], topics[w]["subs_prob"], sep='\t')
-"""
+# for w in range(len(topics)):
+   # print("\n")
+   # print(topics[w]["topic"], topics[w]["prob"], topics[w]["subs"], topics[w]["subs_prob"], sep='\t')
