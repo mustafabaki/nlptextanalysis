4708438f53157b32fcf636534f2a4d8cee9835a1
Bahadir Akdemir <74659903+BahadirAkdemir@users.noreply.github.com>
2021-06-13 19:13:49 +0300

    Update tr_lda_choice.py

--- tr_lda_choice.py
+++ tr_lda_choice.py
@@ -1,13 +1,22 @@
 class result:
+    """
+    holds topic names and their proportions
+    """
     def __init__(self, topic, score):
         self.topic = topic
         self.score = score
 
 
 def make_ngrams(texts,n,ngram_mod):
+    """
+    sends each text to turmod function and returns its outputs as a list
+    """
     return [turnmod(doc,n,ngram_mod) for doc in texts]
 
 def lemmatization(texts):
+    """
+    Checks each word, make them lemmatized and returns lemmatized words as strings
+    """
       texts_out = []
       for sent in texts:
           x=analyzer.analyze(sent)[0][0]
@@ -20,6 +29,9 @@ def lemmatization(texts):
 
 
 def clean(df):
+    """
+    Cleans the data from some noisy elements (it can be change according to the data sent)
+    """
   df.fillna('').astype(str)
   df=df.astype(str)
   df = df.map(lambda x: re.sub('[,\.!?();:$%&#"]', '', x))
@@ -30,6 +42,9 @@ def clean(df):
   return df 
 
 def prepare_stopwords(link):
+    """
+    Creates stopword list. If there is no specialized stopwors, then it uses default document.
+    """"
   stop_word_list=pd.read_csv(link)
   stop_word_list=stop_word_list.values.tolist()
   stopwords=[]
@@ -63,6 +78,9 @@ import numpy as np
 
 
 def f(DATA_LINK, DATA_COLUMN_NAME, STOPWORD_CHOICE, STOPWORD_LINK, NGRAM_CHOICE,NGRAM_NUM, TestData,topic_number_user,fetchArray):
+    """
+    Main function to be called in interface
+    """
     data = pd.read_csv(DATA_LINK)
     df=data[DATA_COLUMN_NAME]
     ######################################################################
@@ -140,12 +158,18 @@ def f(DATA_LINK, DATA_COLUMN_NAME, STOPWORD_CHOICE, STOPWORD_LINK, NGRAM_CHOICE,
     fetchArray.put(topics)
 
 def turnmod(text,n,ngram_mod):
+    """
+    Completes ngrams cumulatively according to the amount of given ngrams
+    """
     data_gram=ngram_mod[0][text]
     for i in range(n-1):
       data_gram=ngram_mod[i+1][data_gram]
     return data_gram
 
 def preprocess(text,stopwords):
+    """
+    Filters the words according to stopwords and their length.
+    """
     result=[]
     for token in gensim.utils.simple_preprocess(text) :
         if token not in stopwords and len(token) > 3:
